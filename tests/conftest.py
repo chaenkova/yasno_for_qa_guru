faca4b3554e09541db5727a0c3f5bfab34fe122a
Daria Chaenkova <dash.kustowa2010@yandex.ru>
2024-08-02 11:51:37 +0300

    fix session id

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,6 +32,11 @@ def browser_settings():
         }
     }
 
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--incognito")
+
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
