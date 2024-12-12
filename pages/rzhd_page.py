82631a0861f847cbeb9f2650fb9d97b434a595c0
Daria Chaenkova <dash.kustowa2010@yandex.ru>
2024-12-12 18:45:19 +0300

    update mobile

---
+++ pages/rzhd_page.py
@@ -0,0 +1,93 @@
+
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have, be
+from datetime import date
+import os
+
+
+class RzhdPage:
+
+    @staticmethod
+    def open_from_station():
+        with step(f'Открываем поиск станций отправления'):
+            browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/fromStationLayout")).click()
+
+    @staticmethod
+    def type_city(city):
+        with step(f'Вбиваем город {city}'):
+            browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).type(f"{city}")
+
+    @staticmethod
+    def click_city(city):
+        with step('Выбираем Москву'):
+            browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
+                             f'new UiSelector().text(\"{city}\")')).click()
+
+    @staticmethod
+    def open_to_station(station):
+        with step(f'Открываем поиск станций прибытия ({station})'):
+            browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toStationLayout")).click()
+        with step('Выьбираем Казань'):
+            browser.element((AppiumBy.ANDROID_UIAUTOMATOR, f'new UiSelector().text(\"{station}\")')).click()
+
+    @staticmethod
+    def chose_today_date():
+        with step('Открываем datapicker'):
+            browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/dateLayout")).click()
+        with step('Кликаем сегодняшнюю дату'):
+            browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/date")).click()
+
+    @staticmethod
+    def click_search():
+        with step('Нажимаем поиск'):
+            browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).click()
+
+    @staticmethod
+    def check_from_to_today(station_from, station_to):
+        with step(f'На экране поезд {station_from}-{station_to}, отправление сегодня'):
+            assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toolbarStationFrom")).should(
+                have.text(f'{station_from}'))
+            assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toolbarStationTo")).should(
+                have.text(f'{station_to}'))
+
+            assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toolbarDate")).should(
+                have.text(date.today().strftime("%d")))
+
+    @staticmethod
+    def check_all_station_of_city():
+        with step('Можно выбрать поезда на всех вокзалах города'):
+            assert browser.element((AppiumBy.ID,
+                                    f"{os.getenv('APP_NAME')}/all_stations_bage")).should(have.text('все вокзалы'))
+
+    @staticmethod
+    def check_duplicates():
+        with step('Есть похожие станции'):
+            assert browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
+                                    'new UiSelector().text(\"Похожие станции\")')).should(be.existing)
+
+    @staticmethod
+    def clean_field_from():
+        with step('Очищаем поле'):
+            browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/text_input_end_icon")).click()
+
+    @staticmethod
+    def check_clean_field():
+        with step('Поле очищено'):
+            assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).should(
+                have.text('Станция отправления'))
+
+    @staticmethod
+    def chose_fly():
+        with step('Выбор авиабилетов'):
+            browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
+                             'new UiSelector().text(\"Самолеты\")')).click()
+
+    @staticmethod
+    def check_fly_popup():
+        with step('Текст с вопросом об авиабилетах'):
+            assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/text")).should(
+                have.text('Стоит ли нам сделать продажу авиа билетов?'))
+
+
+rzhd = RzhdPage()
--- tests/mobile_tests/mobile_test.py
+++ tests/mobile_tests/mobile_test.py
@@ -1,73 +1,37 @@
 import pytest
-from allure_commons._allure import step
-from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, have, be
-from datetime import date
-import os
+from pages.rzhd_page import rzhd
 
 
 def test_android_search_today():
-    with step('Открываем поиск станций отправления'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/fromStationLayout")).click()
-    with step('Выбираем Москву'):
-        browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
-                         'new UiSelector().text(\"Москва\")')).click()
-    with step('Открываем поиск станций прибытия'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toStationLayout")).click()
-    with step('Выьбираем Казань'):
-        browser.element((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text(\"Казань\")')).click()
-    with step('Открываем datapicker'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/dateLayout")).click()
-    with step('Кликаем сегодняшнюю дату'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/date")).click()
-    with step('Нажимаем поиск'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).click()
-    with step('На экране поезд Москва-Казань, отправление сегодня'):
-        assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toolbarStationFrom")).should(have.text('Москва'))
-        assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toolbarStationTo")).should(have.text('Казань'))
-
-        assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/toolbarDate")).should(
-            have.text(date.today().strftime("%d")))
+    rzhd.open_from_station()
+    rzhd.click_city("Москва")
+    rzhd.open_to_station("Казань")
+    rzhd.chose_today_date()
+    rzhd.click_search()
+    rzhd.check_from_to_today("Москва","Казань")
 
 
-@pytest.mark.parametrize("city", ['Брянск', 'Казань', 'Неверленд'])
+@pytest.mark.parametrize("city", ['Брянск', 'Казань', pytest.param('Неверленд', marks=pytest.mark.xfail)])
 def test_android_rzhd_search_terminal(city):
-    with step('Открываем поиск станций отправления'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/fromStationLayout")).click()
-    with step('Вбиваем город Брянск'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).type("Брянск")
-    with step('Можно выбрать поезда на всех вокзалах города'):
-        assert browser.element((AppiumBy.ID,
-                                f"{os.getenv('APP_NAME')}/all_stations_bage")).should(have.text('все вокзалы'))
+    rzhd.open_from_station()
+    rzhd.type_city(city)
+    rzhd.check_all_station_of_city()
 
 
 @pytest.mark.parametrize("city", ['Брянск'])
 def test_android_rzhd_search_city_duplicates(city):
-    with step('Открываем поиск станций отправления'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/fromStationLayout")).click()
-    with step('Вбиваем город'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).type(city)
-    with step('Есть похожие станции'):
-        assert browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
-                                'new UiSelector().text(\"Похожие станции\")')).should(be.existing)
+    rzhd.open_from_station()
+    rzhd.type_city(city)
+    rzhd.check_duplicates()
 
 
 def test_android_rzhd_clear_search_field():
-    with step('Открываем поиск станций отправления'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/fromStationLayout")).click()
-    with step('Вбиваем город Брянск'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).type("Брянск")
-    with step('Очищаем поле'):
-        browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/text_input_end_icon")).click()
-    with step('Поле очищено'):
-        assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/search")).should(
-            have.text('Станция отправления'))
+    rzhd.open_from_station()
+    rzhd.type_city('Брянск')
+    rzhd.clean_field_from()
+    rzhd.check_clean_field()
 
 
 def test_android_flies_waiting_text_is_present():
-    with step('Выбор авиабилетов'):
-        browser.element((AppiumBy.ANDROID_UIAUTOMATOR,
-                         'new UiSelector().text(\"Самолеты\")')).click()
-    with step('Текст с вопросом об авиабилетах'):
-        assert browser.element((AppiumBy.ID, f"{os.getenv('APP_NAME')}/text")).should(
-            have.text('Стоит ли нам сделать продажу авиа билетов?'))
+    rzhd.chose_fly()
+    rzhd.check_fly_popup()
